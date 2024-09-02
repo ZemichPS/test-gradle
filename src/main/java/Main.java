2dd7d355b7cbd66c35e45c0496ccab1eaad4fa9d
Eugene <compserv@yahoo.com>
2024-09-02 20:29:39 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import model.Address;
+import model.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+
+    }
+}
---
+++ src/main/java/model/Cat.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Cat {
+    String name;
+    int age;
+}
