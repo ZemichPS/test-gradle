51e56c95afbb0a92550df33354ebeee12f7cfc6c
Eugene <compserv@yahoo.com>
2024-09-02 14:30:24 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import model.Address;
+import model.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User();
+        User user1=  new User();
+        Address address = new Address();
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,64 @@
+package model;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    private Address address;
+
+    public User(int id,
+                String name,
+                int age,
+                String gender, Address address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
